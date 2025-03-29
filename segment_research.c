489b0389c1ee716b63627ab9218d9dda69900292
EduarddIvanov <ivanov.edward@lll.kpi.ua>
2025-03-29 13:36:29 +0200

    Update segment_research.c

--- segment_research.c
+++ segment_research.c
@@ -2,26 +2,21 @@
 #include <stdlib.h>
 
 int global_array[1000];
-
 int global_init_array[1000] = {[0 ... 999] = 42};
 
 void hello_world_demo() {
     printf("Hello, World!\n");
 }
-
 void local_array_demo() {
     int local_uninit_array[1000];
     int local_init_array[1000] = {[0 ... 999] = 55};
-
     printf("Локальний неініційований масив: %p\n", (void*)local_uninit_array);
     printf("Локальний ініційований масив: %p\n", (void*)local_init_array);
 }
-
 void segment_address_demo() {
-    static int static_var = 100;            // Data сегмент
-    static int static_uninit_var;           // BSS сегмент
-    int *heap_var = malloc(sizeof(int));    // Heap (динамічна пам'ять)
-
+    static int static_var = 100;            
+    static int static_uninit_var;           
+    int *heap_var = malloc(sizeof(int));    
     printf("\nАдреси різних сегментів:\n");
     printf("Глобальний неініційований масив (BSS): %p\n", (void*)global_array);
     printf("Глобальний ініційований масив (Data): %p\n", (void*)global_init_array);
@@ -29,7 +24,6 @@ void segment_address_demo() {
     printf("Статична неініційована змінна (BSS): %p\n", (void*)&static_uninit_var);
     printf("Динамічно виділена пам'ять (Heap): %p\n", (void*)heap_var);
     printf("Адреса функції (Text): %p\n", (void*)hello_world_demo);
-
     free(heap_var);
 }
 
@@ -37,6 +31,5 @@ int main() {
     hello_world_demo();
     local_array_demo();
     segment_address_demo();
-
     return 0;
 }
