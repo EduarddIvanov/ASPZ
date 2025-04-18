99e567c36455a91e65a39708232ee75baa847102
EduarddIvanov <ivanov.edward@lll.kpi.ua>
2025-04-18 17:04:59 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int array[5] = {10, 20, 30, 40, 50};
+    for (int i = 0; i < 10; i++) {
+        printf("array[%d] = %d\n", i, array[i]);
+    }
+
+    return 0;
+}
